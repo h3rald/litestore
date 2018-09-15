653ba94bfdbc97ef425a3075c11e01e7e0fd6814
h3rald <h3rald@h3rald.com>
2018-09-15 13:51:20 +0200

    Fixed default url.

--- lib/types.nim
+++ lib/types.nim
@@ -93,7 +93,7 @@ var
 
 PEG_TAG = peg"""^\$? [a-zA-Z0-9_\-?~:.@#^!+]+$"""
 PEG_USER_TAG = peg"""^[a-zA-Z0-9_\-?~:.@#^!+]+$"""
-PEG_DEFAULT_URL = peg"""^\/{(docs / info / dir)} (\/ {(.+)} / \/?)$"""
+PEG_DEFAULT_URL = peg"""^\/{(docs / info / dir / tags)} (\/ {(.+)} / \/?)$"""
 PEG_URL = peg"""^\/({(v\d+)} \/) {([^\/]+)} (\/ {(.+)} / \/?)$"""
 
 # Initialize LiteStore
