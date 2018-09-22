3298b52f01d5588aaddb4677f6733cafc9e28edf
h3rald <h3rald@h3rald.com>
2018-09-22 22:44:18 +0200

    Added indicator for mirroring changes.

--- src/admin/js/components/info.js
+++ src/admin/js/components/info.js
@@ -16,11 +16,13 @@
       }
     };
     var readonly = info.read_only ? m("span.label.label-success", "Yes") : m("span.label.label-danger", "No"); 
+    var mirror = info.mount ? m("span.label.label-success", "Yes") : m("span.label.label-danger", "No"); 
     var infolist = m(".col-sm-6", [m("ul.list-unstyled", [
           li("Version", info.version),
           li("Datastore Version", info.datastore_version),
           li("Size", info.size),
-          li("Mounted Directory", info.directory, info.directory === null),
+          li("Serving Directory", info.directory, info.directory === null),
+          li("Mirroring Changes", mirror),
           li("Log Level", info.log_level),
           li("Read-Only", readonly),
           li("Total Documents", m("span.badge", info.total_documents)),
