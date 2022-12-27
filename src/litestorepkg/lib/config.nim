005e8cf145b1f3c9f10c7725d94b19628d739706
h3rald <h3rald@h3rald.com>
2022-12-27 16:22:43 +0100

    Incremented version.

--- src/litestorepkg/lib/config.nim
+++ src/litestorepkg/lib/config.nim
@@ -1,6 +1,6 @@
 const
   pkgName*        = "litestore"
-  pkgVersion*     = "1.12.0"
+  pkgVersion*     = "1.12.1"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "Self-contained, lightweight, RESTful document store."
   pkgLicense*     = "MIT"
